2ff0db05a95eab3c620a026d000beca2ad8346a9
Ghassan Al Karaan <ghass.karaan@gmail.com>
2023-10-17 12:56:46 +0300

    update

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 
 // * Application class
 class MyApp extends StatelessWidget {
+  // * Factory constructor
+  // factory MyApp() => instance;
+  factory MyApp() {
+    return instance;
+  }
+
   // * Named constructor - we don't need it
   // const MainApp({super.key});
 
   // * Private named constructor
   // const MyApp._internal();
   MyApp._internal();
-  int appState = 0; //var
-  
 
   // * Single instance - singleton
   //static const MyApp instance = MyApp._internal();
   static final MyApp instance = MyApp._internal();
 
-  // * Factory constructor
-  // factory MyApp() => instance;
-  factory MyApp() {
-    return instance;
-  }
+  int appState = 0; //var
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = Color(int.parse("0xFFE98E21"));
+}
